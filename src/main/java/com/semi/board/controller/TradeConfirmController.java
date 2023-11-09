9f8de15d8abe7590ba35bf191ea2d90802ca21ca
dglimmm <nathomme@naver.com>
2023-11-09 17:27:15 +0900

    아진짜

--- src/main/java/com/semi/board/controller/TradeConfirmController.java
+++ src/main/java/com/semi/board/controller/TradeConfirmController.java
@@ -31,7 +31,6 @@ public class TradeConfirmController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-<<<<<<< HEAD
 		request.setCharacterEncoding("UTF-8");
 		int boardNo = Integer.parseInt(request.getParameter("bno"));
 		int rWriter = Integer.parseInt(request.getParameter("rwriter"));
